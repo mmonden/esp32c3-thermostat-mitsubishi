1241add678992f981c018fabcae70a07cb19243b
JDogM <matt.mll@hotmail.com>
2023-03-09 09:31:04 +0100

    working homekit thermostat

---
+++ DEV_LED.h
@@ -0,0 +1,22 @@
+struct DEV_LED : Service::LightBulb 
+{               // First we create a derived class from the HomeSpan LightBulb Service
+  int ledPin;                                       // this variable stores the pin number defined for this LED
+  SpanCharacteristic *power;                        // here we create a generic pointer to a SpanCharacteristic named "power" that we will use below
+
+  // Next we define the constructor for DEV_LED.  Note that it takes one argument, ledPin,
+  // which specifies the pin to which the LED is attached.
+  
+  DEV_LED(int ledPin) : Service::LightBulb()
+  {
+    power = new Characteristic::On();                 // this is where we create the On Characterstic we had previously defined in setup().  Save this in the pointer created above, for use below
+    this->ledPin = ledPin;                            // don't forget to store ledPin...
+    pinMode(ledPin,OUTPUT);                         // ...and set the mode for ledPin to be an OUTPUT (standard Arduino function)
+  } // end constructor
+
+  boolean update()
+  {            
+    digitalWrite(ledPin,power->getNewVal());        // use a standard Arduino function to turn on/off ledPin based on the return of a call to power->getNewVal() (see below for more info)
+   
+    return(true);                                   // return true to indicate the update was successful (otherwise create code to return false if some reason you could not turn on the LED)
+  } // update
+};
---
+++ DEV_Thermostat.h
@@ -0,0 +1,88 @@
+// #include "Span.h"
+// #include "HomeSpan.h"
+
+enum TargetHeaterStates {
+  OFF = 0,
+  HEAT = 1,
+  COOL = 2,
+  AUTO = 3
+};
+
+enum CurrentHeaterStates {
+  IDLE = 0,
+  HEATING = 1,
+  COOLING = 2
+};
+
+struct DEV_Thermostat : public Service::Thermostat
+{
+private:
+	// TargetHeaterStates *heatPumpState = (TargetHeaterStates *) TargetHeaterStates::OFF;
+	TargetHeaterStates heatPumpState = TargetHeaterStates::OFF;
+
+	bool wasUpdated;
+
+	int readPin;
+	float temperature;
+	unsigned long lastUpdateTime, updatePeriod;
+
+	SpanCharacteristic *currentState;
+	SpanCharacteristic *targetState;
+	SpanCharacteristic *currentTemperature;
+	SpanCharacteristic *targetTemperature;
+	SpanCharacteristic *unit;
+
+	void getTemperature(float *t, int pin)
+	{
+		int loop = 400;
+		float adc = 0.0;
+
+		for(int i = 0; i < loop; i++)
+			adc += analogReadMilliVolts(pin);
+
+		*t = adc/loop*0.1 - 2;
+	}
+
+	void setTemperature(float *t)
+	{
+		currentTemperature->setVal(*t);
+	}
+
+public:
+	DEV_Thermostat(int readPin, unsigned long updatePeriod) : Service::Thermostat()
+	{
+		getTemperature(&temperature, readPin);
+		this->updatePeriod = updatePeriod;
+		this->readPin = readPin;
+
+		currentState = new Characteristic::CurrentHeatingCoolingState(CurrentHeaterStates::IDLE);
+		targetState = new Characteristic::TargetHeatingCoolingState(heatPumpState, true);
+		currentTemperature = new Characteristic::CurrentTemperature(temperature);
+		targetTemperature = new Characteristic::TargetTemperature(22, true);
+		unit = new Characteristic::TemperatureDisplayUnits(0);
+
+		targetState->setValidValues(4, TargetHeaterStates::OFF, TargetHeaterStates::HEAT, TargetHeaterStates::COOL, TargetHeaterStates::AUTO);
+		currentState->setValidValues(3, CurrentHeaterStates::IDLE, CurrentHeaterStates::HEATING, CurrentHeaterStates::COOLING);
+		currentTemperature->setRange(-10, 40, 0.1);
+		targetTemperature->setRange(10, 30, 0.5);
+
+		lastUpdateTime = millis();
+		wasUpdated = false;
+	}
+
+	virtual bool update() override
+	{lmkmmllkmlkmlkmjkmkjllkmmlkmj
+		wasUpdated = targetState->updated() || targetTemperature->updated();
+
+		return true;
+	}
+
+	virtual void loop() override
+	{
+		if((millis() - lastUpdateTime) > updatePeriod)
+		{
+			getTemperature(&temperature, readPin);
+			setTemperature(&temperature);
+		}
+	}
+};
